56d1628a6c62d68f59cf0e50bdbaf4f4dc449a2a
harveydgb <77412053+harveydgb@users.noreply.github.com>
2025-11-27 16:11:04 +0000

    finished sectioning q2i

--- s1_sol/q1.py
+++ s1_sol/q1.py
@@ -11,6 +11,26 @@ import os
 import matplotlib.cm as cm      
 import matplotlib.colors as mcolors
 
+# remove this #
+def update_results_json(results, section, filepath='../results.json'):
+    """
+    Updates a section in the results.json file with the results file passed.
+    """
+    #opening
+    with open(filepath, 'r') as f:
+        data = json.load(f)
+
+    #entering data
+    for key, (val, err) in results.items():
+        data[section]["values"][key] = float(val)
+        data[section]["errors"][key] = float(err)
+
+    #saving
+    with open(filepath, 'w') as f:
+        json.dump(data, f, indent=4)
+
+    print("Saved to 'results.json'")
+
 # Question 1) (i)
 def plot_total_hist(df):
     """
@@ -58,178 +78,6 @@ def plot_overlapping_hist(df):
     return fig, ax
 
 # Question 1) (iii)
-# def plot_sample_estimates(df):
-#     """
-#     Calculating and plotting the samples estimates and associated estimated 
-#     errors of the mean and standard deviation.
-#     """
-    
-#     grouped = df.groupby('E_true')['E_rec']
-
-#     #calculating samples values and associated errors
-#     mu_samp = grouped.mean()
-#     sigma_samp = grouped.std(ddof=1)
-#     N = grouped.count()
-
-#     mu_error = sigma_samp/ (N ** 0.5)
-#     sigma_error = sigma_samp/( (2*(N-1)) ** 0.5)
-
-    
-#     #plotting subplots
-#     fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12.8, 4.8))
-    
-#     #samples mean
-#     ax1.errorbar(mu_samp.index, mu_samp, yerr=mu_error, fmt='o', capsize=4,color='black')
-#     ax1.set_xlabel(r"$E_0$ [GeV]")
-#     ax1.set_ylabel(r"Mean Energy $\hat{\mu}_{\rm samp}$ [GeV]")
-#     ax1.set_title(r"Sample Mean vs $E_0$")
-#     ax1.grid(True, linestyle='--', alpha=0.5)
-    
-#     #sample standard deviation
-#     ax2.errorbar(sigma_samp.index, sigma_samp, yerr=sigma_error, fmt='o', capsize=4,color='black')
-#     ax2.set_xlabel(r"$E_0$ [GeV]")
-#     ax2.set_ylabel(r"Standard Deviation $\hat{\sigma}_{\rm samp}$ [GeV]")
-#     ax2.set_title(r"Sample Standard Deviation vs $E_0$")
-#     ax2.grid(True, linestyle='--', alpha=0.5)
-    
-#     return fig, (mu_samp, mu_error, sigma_samp, sigma_error)
-
-# # Question 1) (iv)
-# def plot_fitted_sample_estimates(values, n_boot=1000):
-#     """Applies and plots a least squares fit to the range of sample mmeans 
-#     and standard deviations across the E_0 value range."""
-
-#     ### fitting values ###
-#     #unpacking values
-#     mu_samp, mu_error, sigma_samp, sigma_error = values
-    
-#     #defining functions for curve fitting
-#     def mean_func(E_0, lam, delta):
-#         return lam * E_0 + delta
-
-#     def sigma_func(E_0, a, b, c):
-#         return np.sqrt(np.abs((E_0 * (a**2)) + (b**2) + ((E_0**2) * (c**2))))
-
-#     #fitting mean
-#     mean_params, mean_params_cov = curve_fit(mean_func, mu_samp.index.values, mu_samp.values, sigma = mu_error.values, absolute_sigma=True)
-#     mean_params_error = np.sqrt(np.diag(mean_params_cov))
-
-#     #fitting standard deviation
-#     p0_sig = [0.5, 1, 0.01] 
-#     sigma_params, sigma_params_cov = curve_fit(sigma_func, sigma_samp.index.values, sigma_samp.values, sigma=sigma_error.values, p0 = p0_sig, absolute_sigma=True)
-#     sigma_params_error = np.sqrt(np.diag(sigma_params_cov))
-
-#     #printing out fitted values
-#     print("Fitted parameter values:")
-#     print(f"λ = {mean_params[0]:.3f} ± {mean_params_error[0]:.3f}")
-#     print(f"Δ = {mean_params[1]:.3f} ± {mean_params_error[1]:.3f}")
-#     print(f"a = {sigma_params[0]:.3f} ± {sigma_params_error[0]:.3f}")
-#     print(f"b = {sigma_params[1]:.3f} ± {sigma_params_error[1]:.3f}")
-#     print(f"c = {sigma_params[2]:.3f} ± {sigma_params_error[2]:.3f} \n")
-
-#     #saving and returning results in a dictionary
-#     results = {
-#         "lb": (mean_params[0], mean_params_error[0]),
-#         "dE": (mean_params[1], mean_params_error[1]),
-#         "a":  (sigma_params[0], sigma_params_error[0]),
-#         "b":  (sigma_params[1], sigma_params_error[1]),
-#         "c":  (sigma_params[2], sigma_params_error[2]),
-#     }
-
-#     ### calculating error bands by bootstrapping ###
-
-#     #creating x array to sample y values over
-#     x_arr = np.linspace(min(mu_samp.index.values), max(mu_samp.index.values), 200)
-
-#     boot_mean_curves = []
-#     boot_sigma_curves = []
-
-#     #parametric bootstrapping
-#     for i in range(n_boot):
-#         #creating resamples
-#         mu_resamp = np.random.normal(mu_samp.values, mu_error.values)
-#         sigma_resamp = np.random.normal(sigma_samp.values, sigma_error.values)
-
-#         #fitting
-#         boot_mean_params, _ = curve_fit(mean_func, mu_samp.index.values, mu_resamp, sigma = mu_error.values, absolute_sigma=True)
-#         boot_sigma_params, _ = curve_fit(sigma_func, sigma_samp.index.values, sigma_resamp, sigma=sigma_error.values, p0 = p0_sig, absolute_sigma=True)
-
-#         #saving to list
-#         boot_mean_curves.append(mean_func(x_arr, *boot_mean_params) - x_arr)
-#         boot_sigma_curves.append(sigma_func(x_arr, *boot_sigma_params) / x_arr)
-
-#     #calculating std. deviations for both mean and sigma from bootstrapping
-#     mean_std = np.std(boot_mean_curves,axis=0)
-#     sigma_std = np.std(boot_sigma_curves,axis=0)
-
-#     #creating y values for fitted curves
-#     fitted_mean = (mean_func(x_arr, *mean_params) - x_arr)
-#     fitted_sigma = (sigma_func(x_arr, *sigma_params) / x_arr)
-
-
-#     ### plotting samples and fitted curves ###
-
-#     fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12.8, 4.8))
-
-#     #mean plot
-#     mu_samp_scaled = mu_samp.values - mu_samp.index.values
-    
-#     ax1.errorbar(mu_samp.index.values, mu_samp_scaled, yerr=mu_error.values, fmt='o', label='Data', capsize=4,color='black')
-#     ax1.plot(x_arr, fitted_mean, 'r-', label='Fit')
-#     ax1.fill_between(x_arr, fitted_mean - mean_std, fitted_mean + mean_std, color='r', alpha=0.3, label=r'$\pm 1\sigma$ Band')
-    
-#     ax1.set_xlabel(r"$E_0$ [GeV]")
-#     ax1.set_ylabel(r"$\hat{\mu}_{\rm samp} - E_0$ [GeV]")
-#     ax1.set_title("Linearity Check")
-#     ax1.legend()
-#     ax1.grid(True, linestyle='--', alpha=0.5)
-
-#     #sigma plot
-#     sigma_samp_scaled = sigma_samp.values / sigma_samp.index.values
-#     sigma_err_scaled = sigma_error.values / sigma_samp.index.values
-    
-#     ax2.errorbar(sigma_samp.index.values, sigma_samp_scaled, yerr=sigma_err_scaled, fmt='o', label='Data', capsize=4,color='black')
-#     ax2.plot(x_arr, fitted_sigma, 'r-', label='Fit')
-#     ax2.fill_between(x_arr, fitted_sigma - sigma_std, fitted_sigma + sigma_std, color='r', alpha=0.3, label=r'$\pm 1\sigma$ Band')
-
-#     ax2.set_xlabel(r"$E_0$ [GeV]")
-#     ax2.set_ylabel(r"$\hat{\sigma}_{\rm samp} / E_0$")
-#     ax2.set_title("Fractional Resolution")
-#     ax2.legend()
-#     ax2.grid(True, linestyle='--', alpha=0.5)
-    
-#     fig.tight_layout()
-
-#     return results, fig
-
-def update_results_json(results, section, filepath='../results.json'):
-    """
-    Updates a section in the results.json file with the results file passed.
-    """
-    #opening
-    with open(filepath, 'r') as f:
-        data = json.load(f)
-
-    #entering data
-    for key, (val, err) in results.items():
-        data[section]["values"][key] = float(val)
-        data[section]["errors"][key] = float(err)
-
-    #saving
-    with open(filepath, 'w') as f:
-        json.dump(data, f, indent=4)
-
-    print("Saved to 'results.json'")
-
-
-
-
-
-
-
-###### Sectioned up #########
-
-## 1) (iii) plan ##
 def calculate_sample_estimates(df):
     """
     Calculating the samples estimates and associated estimated 
@@ -247,7 +95,6 @@ def calculate_sample_estimates(df):
     sigma_error = sigma_samp/( (2*(N-1)) ** 0.5)
 
     return (mu_samp, mu_error, sigma_samp, sigma_error)
-
 
 def plot_sample_estimates(sample_estimate_values):
     """Takes samples estimate values and plots on two separate histograms.
@@ -282,9 +129,7 @@ def calculate_and_plot_sample_estimates(df):
 
     return fig, sample_estimate_values
 
-
-
-## 1) (iv) plan ##
+# Question 1) (iv)
 
 def mean_func(E_0, lb, dE):
     return lb * E_0 + dE
@@ -453,7 +298,6 @@ def plot_mean_sigma_fit_with_error_bars(sample_estimate_values, param_results, x
     fig.tight_layout()
 
     return fig
-
 
 def least_squares_fit_and_plot(sample_estimate_values):
     """Applies a least squares to fit of the associated functions to the sample 
--- s1_sol/q2.py
+++ s1_sol/q2.py
@@ -12,29 +12,121 @@ import pandas as pd
 from scipy.optimize import curve_fit
 
 # Question 2) (i)
-def plot_unbinned_mle(df):
-    """Performs an unbinned ML fit for each E_0 using iminuit and plots the inidividual fits of each E_0 overlaid."""
+# def plot_unbinned_mle(df):
+#     """Performs an unbinned ML fit for each E_0 using iminuit and plots the inidividual fits of each E_0 overlaid."""
 
-    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12.8, 4.8))
+#     fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12.8, 4.8))
 
-    #creating x array and an empty y array for the total pdf to fill
-    x_arr = np.linspace(-20, 20, 1000)
-    total_summed_pdf = np.zeros_like(x_arr)
+#     #creating x array and an empty y array for the total pdf to fill
+#     x_arr = np.linspace(-20, 20, 1000)
+#     total_summed_pdf = np.zeros_like(x_arr)
+
+#     fit_results = {}
+
+#     #defining function iminuit can read
+#     def gaussian_pdf(x, mu, sigma):
+#         return norm.pdf(x, loc=mu, scale=sigma)
+
+#     #setting histogram plotting colors
+#     unique_energies = sorted(df['E_true'].unique())
+#     norm_color = mcolors.Normalize(vmin=min(unique_energies), vmax=max(unique_energies))
+#     cmap = cm.viridis
+
+#     #defining common bins
+#     common_bins = np.linspace(-20, 20, 50)
+#     bin_width = common_bins[1] - common_bins[0]
+
+#     #iterating over each E_0 group
+#     for E_0, group in df.groupby('E_true'):
+#         data = group['E_rec'].values
+
+#         nll = UnbinnedNLL(data, gaussian_pdf)
+
+#         # providing minuit with guesses of sample stats
+#         m = Minuit(nll, mu=np.mean(data), sigma=np.std(data))
+
+#         #running minimisation and error analysis
+#         m.migrad()
+#         m.hesse()
+
+#         #extracting best fit values
+#         mu_hat = m.values['mu']
+#         sigma_hat = m.values['sigma']
+#         mu_error = m.errors['mu']
+#         sigma_error = m.errors['sigma']
+
+#         fit_results[E_0] = {
+#             'mu': (mu_hat, mu_error), 
+#             'sigma': (sigma_hat, sigma_error)
+#         }
+
+#         residuals = data - E_0
+
+#         #plotting left-hand graph
+#         color = cmap(norm_color(E_0))
+#         counts, bins, _ = ax1.hist(residuals, bins=common_bins, histtype='step', label=f'$E_0={E_0}$', alpha=0.8, color=color)
+#         n_events = len(data)
+
+#         pdf_y = norm.pdf(x_arr, loc=(mu_hat - E_0), scale = sigma_hat)
+
+#         #scale the pdf to match the histogram area
+#         scaled_pdf = pdf_y * (n_events * bin_width)
+
+#         #plotting the curves on the left-hand graph
+#         ax1.plot(x_arr, scaled_pdf, linestyle='--', linewidth=1, alpha = 0.8, color=color)
+
+#         total_summed_pdf += scaled_pdf
+
+#     #plotting right-hand graph
+#     all_diffs = df['E_rec'] - df['E_true']
+#     ax2.hist(all_diffs, bins=common_bins, histtype='step', color='k', label='Total Data', linewidth=1.5)
+#     ax2.plot(x_arr, total_summed_pdf, 'r-', label='Sum of Fits', linewidth=1.5)
+
+#     #extra formatting
+#     ax1.set_xlabel(r"$(E - E_0)$ [GeV]")
+#     ax1.set_ylabel("Frequency")
+#     ax1.set_title("Individual Fits")
+#     ax1.legend()
+    
+#     ax2.set_xlabel(r"$(E - E_0)$ [GeV]")
+#     ax2.set_ylabel("Frequency")
+#     ax2.set_title("Total Data vs. Sum of Fits")
+#     ax2.legend()
+    
+#     fig.tight_layout()
+
+#     ### converting to pandas df format ###
+#     energies = sorted(fit_results.keys())
+    
+#     #extract values into lists
+#     mus = [fit_results[e]['mu'][0] for e in energies]
+#     mu_errs = [fit_results[e]['mu'][1] for e in energies]
+#     sigmas = [fit_results[e]['sigma'][0] for e in energies]
+#     sigma_errs = [fit_results[e]['sigma'][1] for e in energies]
+    
+#     #convert to pandas series
+#     s_mu = pd.Series(mus, index=energies)
+#     s_mu_err = pd.Series(mu_errs, index=energies)
+#     s_sigma = pd.Series(sigmas, index=energies)
+#     s_sigma_err = pd.Series(sigma_errs, index=energies)
+    
+#     #fold into a tuple
+#     values = (s_mu, s_mu_err, s_sigma, s_sigma_err)
+
+#     return values, fig
+    
+
+### section up plan ###
+
+# Question 2) (i)
+
+def unbinned_mle(df):
 
     fit_results = {}
 
     #defining function iminuit can read
     def gaussian_pdf(x, mu, sigma):
         return norm.pdf(x, loc=mu, scale=sigma)
-
-    #setting histogram plotting colors
-    unique_energies = sorted(df['E_true'].unique())
-    norm_color = mcolors.Normalize(vmin=min(unique_energies), vmax=max(unique_energies))
-    cmap = cm.viridis
-
-    #defining common bins
-    common_bins = np.linspace(-20, 20, 50)
-    bin_width = common_bins[1] - common_bins[0]
 
     #iterating over each E_0 group
     for E_0, group in df.groupby('E_true'):
@@ -60,6 +152,51 @@ def plot_unbinned_mle(df):
             'sigma': (sigma_hat, sigma_error)
         }
 
+    ### converting to pandas df format ###
+    energies = sorted(fit_results.keys())
+    
+    #extract values into lists
+    mus = [fit_results[e]['mu'][0] for e in energies]
+    mu_errs = [fit_results[e]['mu'][1] for e in energies]
+    sigmas = [fit_results[e]['sigma'][0] for e in energies]
+    sigma_errs = [fit_results[e]['sigma'][1] for e in energies]
+    
+    #convert to pandas series
+    mu_mle = pd.Series(mus, index=energies)
+    mu_mle_err = pd.Series(mu_errs, index=energies)
+    sigma_mle = pd.Series(sigmas, index=energies)
+    sigma_mle_err = pd.Series(sigma_errs, index=energies)
+    
+    #fold into a tuple
+    mle_fitted_values = (mu_mle, mu_mle_err, sigma_mle, sigma_mle_err)
+
+    return mle_fitted_values
+
+def unbinned_mle_and_histogram_plot(df, mle_fitted_values):
+
+    #unpacking
+    mu_mle, mu_mle_err, sigma_mle, sigma_mle_err = mle_fitted_values
+    
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12.8, 4.8))
+
+    #creating x array and an empty y array for the total pdf to fill
+    x_arr = np.linspace(-20, 20, 1000)
+    total_summed_pdf = np.zeros_like(x_arr)
+    
+    #setting histogram plotting colors
+    unique_energies = sorted(df['E_true'].unique())
+    norm_color = mcolors.Normalize(vmin=min(unique_energies), vmax=max(unique_energies))
+    cmap = cm.viridis
+
+    #defining common bins
+    common_bins = np.linspace(-20, 20, 50)
+    bin_width = common_bins[1] - common_bins[0]
+
+    #iterating over each E_0 group
+    for E_0, group in df.groupby('E_true'):
+        
+        #calculating residual from respective E_0 values
+        data = group['E_rec'].values
         residuals = data - E_0
 
         #plotting left-hand graph
@@ -67,7 +204,8 @@ def plot_unbinned_mle(df):
         counts, bins, _ = ax1.hist(residuals, bins=common_bins, histtype='step', label=f'$E_0={E_0}$', alpha=0.8, color=color)
         n_events = len(data)
 
-        pdf_y = norm.pdf(x_arr, loc=(mu_hat - E_0), scale = sigma_hat)
+
+        pdf_y = norm.pdf(x_arr, loc=(mu_mle[E_0] - E_0), scale = sigma_mle[E_0])
 
         #scale the pdf to match the histogram area
         scaled_pdf = pdf_y * (n_events * bin_width)
@@ -95,24 +233,13 @@ def plot_unbinned_mle(df):
     
     fig.tight_layout()
 
-    ### converting to pandas df format ###
-    energies = sorted(fit_results.keys())
-    
-    #extract values into lists
-    mus = [fit_results[e]['mu'][0] for e in energies]
-    mu_errs = [fit_results[e]['mu'][1] for e in energies]
-    sigmas = [fit_results[e]['sigma'][0] for e in energies]
-    sigma_errs = [fit_results[e]['sigma'][1] for e in energies]
-    
-    #convert to pandas series
-    s_mu = pd.Series(mus, index=energies)
-    s_mu_err = pd.Series(mu_errs, index=energies)
-    s_sigma = pd.Series(sigmas, index=energies)
-    s_sigma_err = pd.Series(sigma_errs, index=energies)
-    
-    #fold into a tuple
-    values = (s_mu, s_mu_err, s_sigma, s_sigma_err)
+    return fig
 
-    return values, fig
-    
+def unbinned_mle_fit_and_plot(df):
+
+    mle_fitted_values = unbinned_mle(df)
+    fig = unbinned_mle_and_histogram_plot(df, mle_fitted_values)
+
+    return fig, mle_fitted_values
+
 
