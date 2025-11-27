271f23eb3b2204509525e48fd9c47430d050b085
harveydgb <77412053+harveydgb@users.noreply.github.com>
2025-11-27 10:56:39 +0000

    finished sectioning q1

--- s1_sol/q1.py
+++ s1_sol/q1.py
@@ -94,113 +94,113 @@ def plot_overlapping_hist(df):
     
 #     return fig, (mu_samp, mu_error, sigma_samp, sigma_error)
 
-# Question 1) (iv)
-def plot_fitted_sample_estimates(values, n_boot=1000):
-    """Applies and plots a least squares fit to the range of sample mmeans 
-    and standard deviations across the E_0 value range."""
+# # Question 1) (iv)
+# def plot_fitted_sample_estimates(values, n_boot=1000):
+#     """Applies and plots a least squares fit to the range of sample mmeans 
+#     and standard deviations across the E_0 value range."""
 
-    ### fitting values ###
-    #unpacking values
-    mu_samp, mu_error, sigma_samp, sigma_error = values
+#     ### fitting values ###
+#     #unpacking values
+#     mu_samp, mu_error, sigma_samp, sigma_error = values
     
-    #defining functions for curve fitting
-    def mean_func(E_0, lam, delta):
-        return lam * E_0 + delta
+#     #defining functions for curve fitting
+#     def mean_func(E_0, lam, delta):
+#         return lam * E_0 + delta
 
-    def sigma_func(E_0, a, b, c):
-        return np.sqrt(np.abs((E_0 * (a**2)) + (b**2) + ((E_0**2) * (c**2))))
+#     def sigma_func(E_0, a, b, c):
+#         return np.sqrt(np.abs((E_0 * (a**2)) + (b**2) + ((E_0**2) * (c**2))))
 
-    #fitting mean
-    mean_params, mean_params_cov = curve_fit(mean_func, mu_samp.index.values, mu_samp.values, sigma = mu_error.values, absolute_sigma=True)
-    mean_params_error = np.sqrt(np.diag(mean_params_cov))
+#     #fitting mean
+#     mean_params, mean_params_cov = curve_fit(mean_func, mu_samp.index.values, mu_samp.values, sigma = mu_error.values, absolute_sigma=True)
+#     mean_params_error = np.sqrt(np.diag(mean_params_cov))
 
-    #fitting standard deviation
-    p0_sig = [0.5, 1, 0.01] 
-    sigma_params, sigma_params_cov = curve_fit(sigma_func, sigma_samp.index.values, sigma_samp.values, sigma=sigma_error.values, p0 = p0_sig, absolute_sigma=True)
-    sigma_params_error = np.sqrt(np.diag(sigma_params_cov))
+#     #fitting standard deviation
+#     p0_sig = [0.5, 1, 0.01] 
+#     sigma_params, sigma_params_cov = curve_fit(sigma_func, sigma_samp.index.values, sigma_samp.values, sigma=sigma_error.values, p0 = p0_sig, absolute_sigma=True)
+#     sigma_params_error = np.sqrt(np.diag(sigma_params_cov))
 
-    #printing out fitted values
-    print("Fitted parameter values:")
-    print(f"λ = {mean_params[0]:.3f} ± {mean_params_error[0]:.3f}")
-    print(f"Δ = {mean_params[1]:.3f} ± {mean_params_error[1]:.3f}")
-    print(f"a = {sigma_params[0]:.3f} ± {sigma_params_error[0]:.3f}")
-    print(f"b = {sigma_params[1]:.3f} ± {sigma_params_error[1]:.3f}")
-    print(f"c = {sigma_params[2]:.3f} ± {sigma_params_error[2]:.3f} \n")
+#     #printing out fitted values
+#     print("Fitted parameter values:")
+#     print(f"λ = {mean_params[0]:.3f} ± {mean_params_error[0]:.3f}")
+#     print(f"Δ = {mean_params[1]:.3f} ± {mean_params_error[1]:.3f}")
+#     print(f"a = {sigma_params[0]:.3f} ± {sigma_params_error[0]:.3f}")
+#     print(f"b = {sigma_params[1]:.3f} ± {sigma_params_error[1]:.3f}")
+#     print(f"c = {sigma_params[2]:.3f} ± {sigma_params_error[2]:.3f} \n")
 
-    #saving and returning results in a dictionary
-    results = {
-        "lb": (mean_params[0], mean_params_error[0]),
-        "dE": (mean_params[1], mean_params_error[1]),
-        "a":  (sigma_params[0], sigma_params_error[0]),
-        "b":  (sigma_params[1], sigma_params_error[1]),
-        "c":  (sigma_params[2], sigma_params_error[2]),
-    }
+#     #saving and returning results in a dictionary
+#     results = {
+#         "lb": (mean_params[0], mean_params_error[0]),
+#         "dE": (mean_params[1], mean_params_error[1]),
+#         "a":  (sigma_params[0], sigma_params_error[0]),
+#         "b":  (sigma_params[1], sigma_params_error[1]),
+#         "c":  (sigma_params[2], sigma_params_error[2]),
+#     }
 
-    ### calculating error bands by bootstrapping ###
+#     ### calculating error bands by bootstrapping ###
 
-    #creating x array to sample y values over
-    x_arr = np.linspace(min(mu_samp.index.values), max(mu_samp.index.values), 200)
+#     #creating x array to sample y values over
+#     x_arr = np.linspace(min(mu_samp.index.values), max(mu_samp.index.values), 200)
 
-    boot_mean_curves = []
-    boot_sigma_curves = []
+#     boot_mean_curves = []
+#     boot_sigma_curves = []
 
-    #parametric bootstrapping
-    for i in range(n_boot):
-        #creating resamples
-        mu_resamp = np.random.normal(mu_samp.values, mu_error.values)
-        sigma_resamp = np.random.normal(sigma_samp.values, sigma_error.values)
+#     #parametric bootstrapping
+#     for i in range(n_boot):
+#         #creating resamples
+#         mu_resamp = np.random.normal(mu_samp.values, mu_error.values)
+#         sigma_resamp = np.random.normal(sigma_samp.values, sigma_error.values)
 
-        #fitting
-        boot_mean_params, _ = curve_fit(mean_func, mu_samp.index.values, mu_resamp, sigma = mu_error.values, absolute_sigma=True)
-        boot_sigma_params, _ = curve_fit(sigma_func, sigma_samp.index.values, sigma_resamp, sigma=sigma_error.values, p0 = p0_sig, absolute_sigma=True)
+#         #fitting
+#         boot_mean_params, _ = curve_fit(mean_func, mu_samp.index.values, mu_resamp, sigma = mu_error.values, absolute_sigma=True)
+#         boot_sigma_params, _ = curve_fit(sigma_func, sigma_samp.index.values, sigma_resamp, sigma=sigma_error.values, p0 = p0_sig, absolute_sigma=True)
 
-        #saving to list
-        boot_mean_curves.append(mean_func(x_arr, *boot_mean_params) - x_arr)
-        boot_sigma_curves.append(sigma_func(x_arr, *boot_sigma_params) / x_arr)
+#         #saving to list
+#         boot_mean_curves.append(mean_func(x_arr, *boot_mean_params) - x_arr)
+#         boot_sigma_curves.append(sigma_func(x_arr, *boot_sigma_params) / x_arr)
 
-    #calculating std. deviations for both mean and sigma from bootstrapping
-    mean_std = np.std(boot_mean_curves,axis=0)
-    sigma_std = np.std(boot_sigma_curves,axis=0)
+#     #calculating std. deviations for both mean and sigma from bootstrapping
+#     mean_std = np.std(boot_mean_curves,axis=0)
+#     sigma_std = np.std(boot_sigma_curves,axis=0)
 
-    #creating y values for fitted curves
-    fitted_mean = (mean_func(x_arr, *mean_params) - x_arr)
-    fitted_sigma = (sigma_func(x_arr, *sigma_params) / x_arr)
+#     #creating y values for fitted curves
+#     fitted_mean = (mean_func(x_arr, *mean_params) - x_arr)
+#     fitted_sigma = (sigma_func(x_arr, *sigma_params) / x_arr)
 
 
-    ### plotting samples and fitted curves ###
+#     ### plotting samples and fitted curves ###
 
-    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12.8, 4.8))
+#     fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12.8, 4.8))
 
-    #mean plot
-    mu_samp_scaled = mu_samp.values - mu_samp.index.values
+#     #mean plot
+#     mu_samp_scaled = mu_samp.values - mu_samp.index.values
     
-    ax1.errorbar(mu_samp.index.values, mu_samp_scaled, yerr=mu_error.values, fmt='o', label='Data', capsize=4,color='black')
-    ax1.plot(x_arr, fitted_mean, 'r-', label='Fit')
-    ax1.fill_between(x_arr, fitted_mean - mean_std, fitted_mean + mean_std, color='r', alpha=0.3, label=r'$\pm 1\sigma$ Band')
+#     ax1.errorbar(mu_samp.index.values, mu_samp_scaled, yerr=mu_error.values, fmt='o', label='Data', capsize=4,color='black')
+#     ax1.plot(x_arr, fitted_mean, 'r-', label='Fit')
+#     ax1.fill_between(x_arr, fitted_mean - mean_std, fitted_mean + mean_std, color='r', alpha=0.3, label=r'$\pm 1\sigma$ Band')
     
-    ax1.set_xlabel(r"$E_0$ [GeV]")
-    ax1.set_ylabel(r"$\hat{\mu}_{\rm samp} - E_0$ [GeV]")
-    ax1.set_title("Linearity Check")
-    ax1.legend()
-    ax1.grid(True, linestyle='--', alpha=0.5)
+#     ax1.set_xlabel(r"$E_0$ [GeV]")
+#     ax1.set_ylabel(r"$\hat{\mu}_{\rm samp} - E_0$ [GeV]")
+#     ax1.set_title("Linearity Check")
+#     ax1.legend()
+#     ax1.grid(True, linestyle='--', alpha=0.5)
 
-    #sigma plot
-    sigma_samp_scaled = sigma_samp.values / sigma_samp.index.values
-    sigma_err_scaled = sigma_error.values / sigma_samp.index.values
+#     #sigma plot
+#     sigma_samp_scaled = sigma_samp.values / sigma_samp.index.values
+#     sigma_err_scaled = sigma_error.values / sigma_samp.index.values
     
-    ax2.errorbar(sigma_samp.index.values, sigma_samp_scaled, yerr=sigma_err_scaled, fmt='o', label='Data', capsize=4,color='black')
-    ax2.plot(x_arr, fitted_sigma, 'r-', label='Fit')
-    ax2.fill_between(x_arr, fitted_sigma - sigma_std, fitted_sigma + sigma_std, color='r', alpha=0.3, label=r'$\pm 1\sigma$ Band')
+#     ax2.errorbar(sigma_samp.index.values, sigma_samp_scaled, yerr=sigma_err_scaled, fmt='o', label='Data', capsize=4,color='black')
+#     ax2.plot(x_arr, fitted_sigma, 'r-', label='Fit')
+#     ax2.fill_between(x_arr, fitted_sigma - sigma_std, fitted_sigma + sigma_std, color='r', alpha=0.3, label=r'$\pm 1\sigma$ Band')
 
-    ax2.set_xlabel(r"$E_0$ [GeV]")
-    ax2.set_ylabel(r"$\hat{\sigma}_{\rm samp} / E_0$")
-    ax2.set_title("Fractional Resolution")
-    ax2.legend()
-    ax2.grid(True, linestyle='--', alpha=0.5)
+#     ax2.set_xlabel(r"$E_0$ [GeV]")
+#     ax2.set_ylabel(r"$\hat{\sigma}_{\rm samp} / E_0$")
+#     ax2.set_title("Fractional Resolution")
+#     ax2.legend()
+#     ax2.grid(True, linestyle='--', alpha=0.5)
     
-    fig.tight_layout()
+#     fig.tight_layout()
 
-    return results, fig
+#     return results, fig
 
 def update_results_json(results, section, filepath='../results.json'):
     """
@@ -309,7 +309,7 @@ def least_squares_fit(sample_estimate_values):
     mean_params_error = np.sqrt(np.diag(mean_params_cov))
 
     #fitting standard deviation
-    p0_sig = [0.5, 1, 0.01] 
+    p0_sig = [0.5, 1, 0.01] #setting sigma param initial values
     sigma_params, sigma_params_cov = curve_fit(sigma_func, sigma_samp.index.values, sigma_samp.values, sigma=sigma_error.values, p0 = p0_sig, absolute_sigma=True)
     sigma_params_error = np.sqrt(np.diag(sigma_params_cov))
 
@@ -339,6 +339,22 @@ def print_and_save_results(param_results, section, filepath='../results.json'):
     print(f"b = {param_results['b'][0]:.3f} ± {param_results['b'][1]:.3f}")
     print(f"c = {param_results['c'][0]:.3f} ± {param_results['c'][1]:.3f} \n")
 
+    ### saving to results.json ###
+    #opening
+    with open(filepath, 'r') as f:
+        data = json.load(f)
+
+    #entering data
+    for key, (val, err) in param_results.items():
+        data[section]["values"][key] = float(val)
+        data[section]["errors"][key] = float(err)
+
+    #saving
+    with open(filepath, 'w') as f:
+        json.dump(data, f, indent=4)
+
+    print("Saved to 'results.json'")
+
 def calculate_error_bands_by_bootstrap(sample_estimate_values, param_results, x_arr, n_boot=1000):
     """Calculating the 1 sigma error bands of the fit using parametric 
     bootstrapping across the E_0 sample range. y-values are re-scaled 
@@ -357,9 +373,10 @@ def calculate_error_bands_by_bootstrap(sample_estimate_values, param_results, x_
     #unpacking values
     mu_samp, mu_error, sigma_samp, sigma_error = sample_estimate_values
 
-
+    #initialising
     boot_mean_curves = []
     boot_sigma_curves = []
+    p0_sig = [0.5, 1, 0.01] #setting sigma param initial values
 
     #parametric bootstrapping
     for i in range(n_boot):
@@ -381,10 +398,28 @@ def calculate_error_bands_by_bootstrap(sample_estimate_values, param_results, x_
 
     return mean_std, sigma_std
 
-def plot_mean_var(sample_estimate_values, x_arr, mean_std, sigma_std, fitted_mean, fitted_sigma):
-    """Plots the """
+def plot_mean_sigma_fit_with_error_bars(sample_estimate_values, param_results, x_arr, mean_std, sigma_std):
+    """Plots two graphs for the mu and sigma values, eaching showing the actual
+    mean and std. dev from E_0, as well the line of best fit, and associated error
+    bands for that fit.
+    
+    Inputs
+    sample_estimate_values: tuple in form of (mu_samp, mu_error, sigma_samp, sigma_error)
+    param_results: dictionary of fit parameter values
+    x_arr: array of smooth x_values across the E_0 value range
+    mean_std: array of uncertainty values for the mean fit at each E_0 value
+    mean_std: array of uncertainty values for the sigma fit at each E_0 value
+    
+    Returns
+    fig: the matplotlib figure object, for purposes of saving the image
+    """
+    
     #unpacking values
     mu_samp, mu_error, sigma_samp, sigma_error = sample_estimate_values
+
+    #creating y values for fitted curves from param results
+    fitted_mean = (mean_func(x_arr, param_results['lb'][0], param_results['dE'][0]) - x_arr)
+    fitted_sigma = (sigma_func(x_arr, param_results['a'][0], param_results['b'][0], param_results['c'][0]) / x_arr)
 
     fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12.8, 4.8))
 
@@ -420,13 +455,33 @@ def plot_mean_var(sample_estimate_values, x_arr, mean_std, sigma_std, fitted_mea
     return fig
 
 
-def least_squares_fit_and_plot():
+def least_squares_fit_and_plot(sample_estimate_values):
+    """Applies a least squares to fit of the associated functions to the sample 
+    estimate values. Then applies bootstrapping to calculate error bands for the 
+    fit at each E_0 value. Plots all of this information onto two graphs, one for
+    mu and one for sigma.
+    
+    Inputs
+    sample_estimate_values: tuple in form of (mu_samp, mu_error, sigma_samp, sigma_error)
+    
+    Returns
+    fig: the matplotlib figure object, for purposes of saving the image
+    """
 
+    #unpacking
+    mu_samp, mu_error, sigma_samp, sigma_error = sample_estimate_values
+
+    #applying least squares fit to functions 
+    param_results = least_squares_fit(sample_estimate_values)
+    print_and_save_results(param_results, "sample_ests", filepath='../results.json')
 
     #creating x array to sample y values over
     x_arr = np.linspace(min(mu_samp.index.values), max(mu_samp.index.values), 200)
+
+    #appling bootstrapping and saving sigma values
+    mean_std, sigma_std = calculate_error_bands_by_bootstrap(sample_estimate_values, param_results, x_arr)
+
+    #plotting
+    fig = plot_mean_sigma_fit_with_error_bars(sample_estimate_values, param_results, x_arr, mean_std, sigma_std)
     
-    #creating y values for fitted curves
-    fitted_mean = (mean_func(x_arr, param_results['lb'][0], param_results['dE'][0]) - x_arr)
-    fitted_sigma = (sigma_func(x_arr, param_results['a'][0], param_results['b'][0], param_results['c'][0]) / x_arr)
-    pass
+    return fig
